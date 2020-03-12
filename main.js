6e2c322d9c863be74ccc52eb1322b414295075c2
talentceffort <talentceffort@gmail.com>
2020-03-12 21:24:17 +0900

    Sagas Basic tutorial complete

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga' // saga import 
 
 import Counter from './Counter'
-import reducer from './reducers'
+import reducer from './reducers/reducers'
+import rootSaga from './sagas/sagas'
 
-const store = createStore(reducer)
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const sagaMiddleware = createSagaMiddleware() // saga 미들웨어 불러오고
+const store = createStore(reducer, devTools, applyMiddleware(sagaMiddleware)) // 추가.
+
+sagaMiddleware.run(rootSaga) // helloSaga 실행시키겠다?
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +24,8 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
     document.getElementById('root')
   )
 }
